d320eaf6cf6d8288006e4fb1a71f73e3cd6d14da
Dena Burd <denaburd@gmail.com>
2020-11-30 19:38:29 -0800

    fix err

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import {connect} from 'react-redux'
-import FileDialog from '../layout/OpenFile'
 import {Link} from 'react-router-dom';
 import {getProjectById} from '../../store/actions/projectActions'
 
